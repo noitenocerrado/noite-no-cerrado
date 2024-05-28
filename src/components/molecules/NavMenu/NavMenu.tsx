bbca025ad7304e6071550b3e6d693ad97ebef60c
Mateus Félix <mateus.cfoliveira@gmail.com>
2024-05-28 20:00:38 -0300

    refactor(NavMenu): improve menu functionality and layout

--- src/components/molecules/NavMenu/NavMenu.tsx
+++ src/components/molecules/NavMenu/NavMenu.tsx
@@ -1,4 +1,9 @@
+'use client'
+
+import Image from 'next/image'
+import { useScroll } from '@/hooks'
 import { LanguageSelector, Logo } from '../../atoms'
+import bgStarsImg from '../../../../public/bg_stars.webp'
 
 const navItemLabelsLeftGroup = [
   { label: 'O Projeto', href: '#project' },
@@ -17,33 +22,65 @@ interface NavItemProps {
 
 const NavItem = ({ label, href }: NavItemProps) => (
   <li className='relative group select-none'>
-    <a href={href} className='hover:text-gray-400 text-xl'>
+    <a href={href} className='hover:text-emerald-300 md:text-xl xs:text-xs'>
       {label}
     </a>
-    <div className='absolute left-0 right-0 h-1 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-out'></div>
+    <div className='absolute left-0 right-0 h-1 bg-emerald-400 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-out'></div>
   </li>
 )
 
-export const NavMenu = () => (
-  <nav className='bg-black text-white py-4'>
-    <div className='container mx-auto flex justify-between items-center'>
-      <ul className='flex space-x-4 flex-1 justify-evenly items-center'>
-        {navItemLabelsLeftGroup.map(({ label, href }) => (
-          <NavItem key={label} label={label} href={href} />
-        ))}
-      </ul>
+export const NavMenu = () => {
+  const { scrolled } = useScroll()
 
-      <div className='flex items-center'>
-        <Logo />
+  return (
+    <nav
+      className={`z-10 fixed w-full bg-black text-white ${
+        scrolled ? 'py-2' : 'py-4'
+      } transition-all duration-300`}
+    >
+      <div className='absolute z-10 inset-0 bg-gradient-to-l from-white/70 to-transparent pointer-events-none' />
+
+      <div
+        className={`absolute z-20 top-2 right-4 transition-opacity duration-300 ${
+          scrolled ? 'opacity-0 pointer-events-none' : 'opacity-100'
+        }`}
+      >
+        <LanguageSelector />
       </div>
 
-      <ul className='flex space-x-4 flex-1 justify-evenly items-center'>
-        {navItemLabelsRightGroup.map(({ label, href }) => (
-          <NavItem key={label} label={label} href={href} />
-        ))}
+      <div className='absolute inset-y-0 left-0 w-2/3 h-full'>
+        <Image
+          priority
+          src={bgStarsImg}
+          alt='Stars Background'
+          layout='fill'
+          objectFit='cover'
+          className='pointer-events-none'
+        />
+        <div className='absolute inset-0 bg-gradient-to-r from-transparent via-90% via-black/80 to-black pointer-events-none' />
+      </div>
 
-        <LanguageSelector />
-      </ul>
-    </div>
-  </nav>
-)
+      <div className='container mt-2 mx-auto flex justify-between items-center relative z-10'>
+        <ul className='flex space-x-4 flex-1 justify-evenly items-center'>
+          {navItemLabelsLeftGroup.map(({ label, href }) => (
+            <NavItem key={label} label={label} href={href} />
+          ))}
+        </ul>
+
+        <div
+          className={`flex items-center transition-all duration-300 ${
+            scrolled ? 'h-10 w-10' : 'h-24 w-24 md:w-48 md:h-48'
+          }`}
+        >
+          <Logo />
+        </div>
+
+        <ul className='flex space-x-4 flex-1 justify-evenly items-center'>
+          {navItemLabelsRightGroup.map(({ label, href }) => (
+            <NavItem key={label} label={label} href={href} />
+          ))}
+        </ul>
+      </div>
+    </nav>
+  )
+}
