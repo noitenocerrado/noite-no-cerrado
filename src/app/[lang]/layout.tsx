6591be7a8bf388de10c5dcaeb0b31edda099d3cb
Mateus Félix <mateus.cfoliveira@gmail.com>
2024-05-28 15:10:39 -0300

    feat: implement i18n | update logo image to .webp | create .prettierrc rules | add pt and en dictionaries

---
+++ src/app/[lang]/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Locale, i18n } from '../../i18n-config'
+import { getDictionary } from '../../getDictionary'
+import '../globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Noite no Cerrado',
+  description:
+    'Projeto de conscientização sobre os efeitos da poluição luminosa no Cerrado brasileiro',
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en',
+      'pt-BR': '/pt',
+    },
+  },
+  openGraph: {
+    url: 'https://noitenocerrado.vercel.app',
+    siteName: 'Noite no Cerrado',
+    images: 'https://noitenocerrado.vercel.app/logo.webp',
+  },
+}
+
+export const generateStaticParams = async () =>
+  i18n.locales.map((locale) => ({ lang: locale }))
+
+const RootLayout = async ({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode
+  params: { lang: Locale }
+}>) => {
+  const dictionary = await getDictionary(params.lang)
+
+  return (
+    <html lang={params.lang}>
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
+
+export default RootLayout
