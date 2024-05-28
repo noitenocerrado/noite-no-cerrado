96c4de7fcc85d4a81779165fa3f729ad99ff41cb
Mateus Félix <mateus.cfoliveira@gmail.com>
2024-05-28 20:27:00 -0300

    refactor(page): add styles to NavMenu implementation

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,9 +1,21 @@
 import { NavMenu } from '@/components'
+import { getDictionary } from '../../getDictionary'
+import { Locale } from '../../i18n-config'
 
-export default function Home() {
+export const Home = async ({
+  params: { lang },
+}: {
+  params: { lang: Locale }
+}) => {
+  const dictionary = await getDictionary(lang)
+
   return (
-    <main className='flex flex-col gap-5 bg-black text-white'>
-      <NavMenu />
+    <main className='flex flex-col gap-5 bg-zinc-950 text-white relative'>
+      <div className='mb-32 md:mb-56'>
+        <NavMenu dictionary={dictionary} />
+      </div>
     </main>
   )
 }
+
+export default Home
