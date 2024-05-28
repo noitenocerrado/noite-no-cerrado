9af276e5009e91d295b55ca302c83156ec7e071b
cometsinthesky <lucasferreiraunb@gmail.com>
2024-05-28 02:59:10 -0300

    create hook useScroll

---
+++ src/hooks/useScroll/useScroll.ts
@@ -0,0 +1,19 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
+export const useScroll = () => {
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 150)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return { scrolled }
+}
