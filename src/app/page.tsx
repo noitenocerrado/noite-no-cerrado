98e5173bff2fbbc12e15b8b7e5e59cefe568edb4
cometsinthesky <lucasferreiraunb@gmail.com>
2024-05-28 03:22:32 -0300

    refactor(page): implement menu click handler | Add project section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
+'use client'
+
 import { Logo, NavMenu } from "@/components";
+import { ProjectSection } from "@/components/organisms";
+import { useState } from "react";
 
 export default function Home() {
+  const [activeSection, setActiveSection] = useState('O Projeto')
+
+  const handleClickMenuItem = (title: string) => {
+    setActiveSection(title)
+  }
+
+  let SectionComponent = <></>
+
+  if (activeSection === 'O Projeto') {
+    SectionComponent = <ProjectSection />
+  } else if (activeSection === 'Fotos') {
+    SectionComponent = <p>Fotos</p>
+  }
+
   return (
     <main className="flex flex-col gap-5 bg-black text-white">
       <Logo />
 
-      <NavMenu />
+      <NavMenu onClickItem={handleClickMenuItem} />
+
+      {/* TODO: Adicionar as sections */}
+      {SectionComponent}
     </main>
   );
 }
