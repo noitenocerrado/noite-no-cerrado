01d9df1b182e8a670393533c4abdcc4713f6a20e
cometsinthesky <lucasferreiraunb@gmail.com>
2024-05-28 03:21:59 -0300

    refactor(NavMenu): implement onClick

--- src/components/molecules/NavMenu/NavMenu.tsx
+++ src/components/molecules/NavMenu/NavMenu.tsx
@@ -1,24 +1,32 @@
+'use client'
+
 interface NavItemProps {
   title: string
+
+  onClick: (title: string) => void
 }
 
-const NavItem = ({ title }: NavItemProps) => (
-  <li className="h-[40px] flex items-center justify-center p-8 cursor-pointer">
+const NavItem = ({ title, onClick }: NavItemProps) => (
+  <li className="h-[40px] flex items-center justify-center p-8 cursor-pointer" onClick={() => onClick(title)}>
     {title}
   </li>
 )
 
-export const NavMenu = () => {
+interface NavMenuProps {
+  onClickItem: (title: string) => void
+}
+
+export const NavMenu = ({ onClickItem }: NavMenuProps) => {
   return (
     <div className="px-4 container mx-auto">
       <nav className="w-full rounded-lg bg-gray-700 text-white transition-transform">
         <div className="w-full flex justify-center">
           <ul className="w-full grid grid-cols-5 divide-x divide-[#69707C]">
-            <NavItem title="O Projeto" />
-            <NavItem title="Fotos" />
-            <NavItem title="Dados" />
-            <NavItem title="Specs" />
-            <NavItem title="Contato" />
+            <NavItem title="O Projeto" onClick={onClickItem} />
+            <NavItem title="Fotos" onClick={onClickItem} />
+            <NavItem title="Dados" onClick={onClickItem} />
+            <NavItem title="Specs" onClick={onClickItem} />
+            <NavItem title="Contato" onClick={onClickItem} />
           </ul>
         </div>
       </nav>
