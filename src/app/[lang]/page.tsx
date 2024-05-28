23f37b5716ac1f11ef7b1f48d7bc1e513b11c067
Mateus Félix <mateus.cfoliveira@gmail.com>
2024-05-28 16:15:30 -0300

    refactor: add style to scroll, remove old code from page.tsx | update logo image size

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,32 +1,9 @@
-'use client'
-
-import { Logo, NavMenu } from "@/components";
-import { ProjectSection } from "@/components/organisms";
-import { useState } from "react";
+import { NavMenu } from '@/components'
 
 export default function Home() {
-  const [activeSection, setActiveSection] = useState('O Projeto')
-
-  const handleClickMenuItem = (title: string) => {
-    setActiveSection(title)
-  }
-
-  let SectionComponent = <></>
-
-  if (activeSection === 'O Projeto') {
-    SectionComponent = <ProjectSection />
-  } else if (activeSection === 'Fotos') {
-    SectionComponent = <p>Fotos</p>
-  }
-
   return (
-    <main className="flex flex-col gap-5 bg-black text-white">
-      <Logo />
-
-      <NavMenu onClickItem={handleClickMenuItem} />
-
-      {/* TODO: Adicionar as sections */}
-      {SectionComponent}
+    <main className='flex flex-col gap-5 bg-black text-white'>
+      <NavMenu />
     </main>
-  );
+  )
 }
--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -1,16 +1,14 @@
 import Image from 'next/image'
 import logoImg from '../../../../public/logo.webp'
 
-export const Logo = () => (
-  <div className='w-full flex justify-center flex-col mt-5 items-center gap-4'>
-    <Image
-      priority
-      width={150}
-      height={150}
-      src={logoImg}
-      alt='logo noite no cerrado'
-    />
+const imageSize = 180
 
-    <p className='text-3xl'>Noite no Cerrado</p>
-  </div>
+export const Logo = () => (
+  <Image
+    priority
+    width={imageSize}
+    height={imageSize}
+    src={logoImg}
+    alt='logo noite no cerrado'
+  />
 )
