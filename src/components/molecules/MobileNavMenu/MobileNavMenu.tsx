2163b50488c6facae285b46e283626755b129df3
Mateus Félix <mateus.cfoliveira@gmail.com>
2024-05-29 20:26:25 -0300

    feat(MobileNavMenu): add title | fix mobile menu button | add menu items

--- src/components/molecules/MobileNavMenu/MobileNavMenu.tsx
+++ src/components/molecules/MobileNavMenu/MobileNavMenu.tsx
@@ -28,22 +28,24 @@ export const MobileNavMenu = ({ dictionary }: TranslationProps) => {
   return (
     <>
       <nav className='fixed w-full flex justify-between items-center'>
+        <button
+          onClick={toggleMenu}
+          className={`absolute top-5 right-4 h-6 flex items-center`}
+        >
+          <span
+            className={`relative z-50 block h-[1px] w-7 bg-transparent content-[''] before:z-50 before:block before:h-full before:w-full before:bg-white before:transition-all before:duration-200 before:ease-out before:content-[''] after:block after:h-full after:w-full after:bg-white after:transition-all after:duration-200 after:ease-out after:content-[''] ${
+              isOpen
+                ? 'bg-transparent before:top-0 before:w-full before:rotate-45 before:transform after:bottom-0 after:w-full after:-rotate-45 after:transform'
+                : 'before:absolute before:top-[-0.35rem] after:absolute after:right-0 after:bottom-[-0.35rem]'
+            }`}
+          />
+        </button>
+
         <div className='z-10 m-4 flex items-center w-10 h-10'>
           <Logo dictionary={dictionary} />
         </div>
 
-        <button
-          onClick={toggleMenu}
-          className={`absolute top-4 right-4 h-6 flex items-center`}
-        >
-          <span
-            className={`relative z-50 block h-[1px] w-7 bg-transparent content-[''] before:absolute before:top-[-0.35rem] before:z-50 before:block before:h-full before:w-full before:bg-white before:transition-all before:duration-200 before:ease-out before:content-[''] after:absolute after:right-0 after:bottom-[-0.35rem] after:block after:h-full after:w-full after:bg-white after:transition-all after:duration-200 after:ease-out after:content-[''] ${
-              isOpen
-                ? 'bg-transparent before:top-1 before:w-full before:rotate-45 before:transform after:bottom-0 after:w-full after:-rotate-45 after:transform'
-                : ''
-            }`}
-          />
-        </button>
+        <p className='z-10 flex-1 text-white text-center text-2xl pr-16'>Noite no Cerrado</p>
 
         <NavBackground dictionary={dictionary} />
 
@@ -58,12 +60,16 @@ export const MobileNavMenu = ({ dictionary }: TranslationProps) => {
             isOpen ? 'translate-x-0' : 'translate-x-full'
           }`}
         >
-          <div className='flex flex-col float-right min-h-full w-[85%] bg-zinc-900 px-6 pt-10 shadow-2xl'>
+          <div className='flex flex-col float-right min-h-full w-[85%] bg-zinc-900 px-6 pt-3 shadow-2xl'>
             <menu>
               <LanguageSelector />
-              {navItems.map((item, index) => (
-                <NavItem key={index} {...item} />
-              ))}
+              <div className='flex flex-col divide-y mt-4'>
+                {navItems.map((item) => (
+                  <div key={item.label} className='py-2'>
+                    <NavItem {...item} />
+                  </div>
+                ))}
+              </div>
             </menu>
           </div>
         </div>
