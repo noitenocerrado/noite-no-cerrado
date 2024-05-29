a9f1bbcc261bd33ac537724bc81ad55af9290ac6
Mateus Félix <mateus.cfoliveira@gmail.com>
2024-05-29 14:06:39 -0300

    refactor(NavMenu): update header background | update stars background

--- src/components/molecules/NavMenu/NavMenu.tsx
+++ src/components/molecules/NavMenu/NavMenu.tsx
@@ -45,7 +45,7 @@ export const NavMenu = ({ dictionary }: TranslationProps) => {
         scrolled ? 'py-2' : 'py-4'
       } transition-all duration-300 ${fadeIn ? 'fade-in-left-to-right' : 'opacity-0'}`}
     >
-      <div className='absolute z-10 inset-0 bg-gradient-to-l from-white/70 to-transparent pointer-events-none' />
+      <div className='absolute z-10 inset-0 bg-black/10 pointer-events-none' />
 
       <div
         className={`absolute z-20 top-2 right-4 transition-opacity duration-300 ${
@@ -55,21 +55,20 @@ export const NavMenu = ({ dictionary }: TranslationProps) => {
         <LanguageSelector />
       </div>
 
-      <div className='absolute inset-y-0 left-0 w-2/3 h-full'>
+      <div className='absolute inset-y-0 left-0 w-full h-full'>
         <Image
           priority
           src={bgStarsImg}
           alt={dictionary.navMenu.bgImageAlt}
           className='pointer-events-none'
           style={{
-            position: 'absolute',
             height: '100%',
             width: '100%',
-            inset: '0px',
             objectFit: 'cover',
           }}
         />
-        <div className='absolute inset-0 bg-gradient-to-r from-transparent via-90% via-black/80 to-black pointer-events-none' />
+        <div className='absolute inset-0 bg-gradient-radial from-transparent via-50% via-black/60 to-black pointer-events-none' />
+        <div className='absolute inset-0 bg-gradient-to-t from-zinc-950 from-10% via-20% via-transparent to-transparent to-90% pointer-events-none' />
       </div>
 
       <div className='container mt-2 mx-auto flex justify-between items-center relative z-10'>
