1cc9e4653deb7ba4a95709398f1feb225939412d
cometsinthesky <lucasferreiraunb@gmail.com>
2024-05-30 02:36:26 -0300

    feat: create component

--- src/components/atoms/SocialIcons/SocialIcons.tsx
+++ src/components/atoms/SocialIcons/SocialIcons.tsx
@@ -1,3 +1,35 @@
-export const SocialIcons = () => {
-  return <>Social Icons</>
+import Link from 'next/link'
+import Image from 'next/image'
+import githubIcon from '../../../../public/github150.png'
+import instagramIcon from '../../../../public/instagram150.png'
+
+const githubLink = 'https://github.com/cometsinthesky/noite-no-cerrado'
+const instagramLink = 'https://www.instagram.com/noitenocerrado'
+
+interface SocialIconsProps {
+  size?: string
+}
+
+export const SocialIcons = ({ size }: SocialIconsProps) => {
+  let width = 20
+  let height = 20
+  let gap = 4
+
+  if (size === 'large') {
+    width = 40
+    height = 40
+    gap = 10
+  }
+
+  return (
+    <div className={`flex gap-${gap} mt-1`}>
+      <Link href={instagramLink} target='_blank'>
+        <Image alt='' src={instagramIcon} width={width} height={height} />
+      </Link>
+
+      <Link href={githubLink} target='_blank'>
+        <Image alt='' src={githubIcon} width={width} height={height} />
+      </Link>
+    </div>
+  )
 }
