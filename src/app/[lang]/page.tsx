5b4ced86f1adf5d32861d12986ac58d3bad25c86
Mateus Félix <mateus.cfoliveira@gmail.com>
2024-05-30 23:53:52 -0300

    feat(page): add footer to page

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,4 +1,4 @@
-import { MobileNavMenu, NavMenu } from '@/components'
+import { Footer, MobileNavMenu, NavMenu } from '@/components'
 import { getDictionary } from '../../getDictionary'
 import { Locale } from '../../i18n-config'
 
@@ -10,13 +10,19 @@ export const Home = async ({
   const dictionary = await getDictionary(lang)
 
   return (
-    <main className='flex flex-col gap-5 bg-zinc-950 text-white relative'>
+    <main className='flex flex-col gap-5 bg-zinc-950 text-white relative h-screen'>
       <div className='hidden sm:block'>
         <NavMenu dictionary={dictionary} />
       </div>
 
       <div className='sm:hidden'>
         <MobileNavMenu dictionary={dictionary} />
+      </div>
+
+      <div className='flex flex-col h-full mt-32 justify-between'>
+        <div className='mb-8'>{/* TODO: Add content sections */}</div>
+
+        <Footer dictionary={dictionary} />
       </div>
     </main>
   )
