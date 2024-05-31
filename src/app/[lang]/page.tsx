5b44d0dd3b7ddb4cafadecc325bb6e8440d43549
Mateus Félix <mateus.cfoliveira@gmail.com>
2024-05-31 20:37:57 -0300

    refactor: extract nav menu to layout

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,6 +3,7 @@ import {
   Footer,
   MobileNavMenu,
   NavMenu,
+  PhotosSection,
   ProjectSection,
 } from '@/components'
 import { Locale } from '../../i18n-config'
@@ -13,17 +14,13 @@ const Home = async ({ params: { lang } }: { params: { lang: Locale } }) => {
 
   return (
     <main className='flex flex-col gap-5 bg-black text-white relative'>
-      <div className='hidden sm:block'>
-        <NavMenu dictionary={dictionary} />
-      </div>
-
-      <div className='sm:hidden'>
-        <MobileNavMenu dictionary={dictionary} />
-      </div>
-
       <div className='flex flex-col px-10 h-full mt-20 sm:px-6 sm:mt-36 justify-between'>
         <div className='mb-16'>
           <ProjectSection dictionary={dictionary} />
+        </div>
+
+        <div className='mb-16'>
+          <PhotosSection dictionary={dictionary} />
         </div>
 
         <div className='mb-16'>
