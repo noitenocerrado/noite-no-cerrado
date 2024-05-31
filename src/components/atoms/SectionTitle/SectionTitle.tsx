9d20da626b8f09349838969340df4e87fa2abd3f
Mateus Félix <mateus.cfoliveira@gmail.com>
2024-05-31 20:09:39 -0300

    refactor(SectionTitle): fix element position

--- src/components/atoms/SectionTitle/SectionTitle.tsx
+++ src/components/atoms/SectionTitle/SectionTitle.tsx
@@ -3,8 +3,8 @@ interface SectionTitleProps {
 }
 
 export const SectionTitle = ({ title }: SectionTitleProps) => (
-  <>
+  <div className="flex flex-col items-center">
     <p className='mb-2 text-3xl md:text-5xl font-title text-center'>{title}</p>
-    <div className='mb-10 h-1 w-96 bg-gradient-radial from-white to-transparent via-transparent mx-auto' />
-  </>
+    <div className='mb-10 h-1 w-full max-w-[600px] bg-gradient-radial from-white to-transparent via-transparent mx-auto' />
+  </div>
 )
