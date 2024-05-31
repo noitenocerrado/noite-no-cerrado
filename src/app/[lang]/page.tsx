f1c6602cf001dffc77a7d83fe71f4d79c910617b
Mateus Félix <mateus.cfoliveira@gmail.com>
2024-05-31 02:11:13 -0300

    feat(page): add AboutUs section to main page

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,6 +1,7 @@
 import { Footer, MobileNavMenu, NavMenu } from '@/components'
-import { getDictionary } from '../../getDictionary'
 import { Locale } from '../../i18n-config'
+import { getDictionary } from '../../getDictionary'
+import { AboutUsSection } from '../../components/organisms'
 
 export const Home = async ({
   params: { lang },
@@ -19,8 +20,10 @@ export const Home = async ({
         <MobileNavMenu dictionary={dictionary} />
       </div>
 
-      <div className='flex flex-col h-full mt-32 justify-between'>
-        <div className='mb-8'>{/* TODO: Add content sections */}</div>
+      <div className='flex flex-col px-10 h-full mt-20 sm:mt-36 md:mt-40 justify-between'>
+        <div className='mb-8'>
+          <AboutUsSection dictionary={dictionary} />
+        </div>
 
         <Footer dictionary={dictionary} />
       </div>
