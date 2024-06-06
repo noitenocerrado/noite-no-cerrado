ef1779a7ef082b1bd136ec859b08bba52c2b992c
Mateus Félix <mateus.cfoliveira@gmail.com>
2024-06-06 10:07:39 -0300

    refactor: add GA Events on click

--- src/components/atoms/SocialIcons/SocialIcons.tsx
+++ src/components/atoms/SocialIcons/SocialIcons.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
 import githubIcon from '../../../../public/github150.png'
 import instagramIcon from '../../../../public/instagram150.png'
+import { sendGAEvent } from '@next/third-parties/google'
 
 const githubLink = 'https://github.com/cometsinthesky/noite-no-cerrado'
 const instagramLink = 'https://www.instagram.com/noitenocerrado'
@@ -24,13 +27,33 @@ export const SocialIcons = ({ size }: SocialIconsProps) => {
     height = 15
   }
 
+  const handleClickSocialIcon = (label: string) => {
+    sendGAEvent('event', 'social_icon', {
+      action: 'click',
+      value: `project_${label}`,
+    })
+  }
+
   return (
     <div className={`flex ${isLarge ? 'gap-10' : 'gap-4'} mt-1`}>
-      <Link href={instagramLink} target='_blank'>
-        <Image alt='Instagram' src={instagramIcon} width={width} height={height} />
+      <Link
+        href={instagramLink}
+        target='_blank'
+        onClick={() => handleClickSocialIcon('instagram')}
+      >
+        <Image
+          alt='Instagram'
+          src={instagramIcon}
+          width={width}
+          height={height}
+        />
       </Link>
 
-      <Link href={githubLink} target='_blank'>
+      <Link
+        href={githubLink}
+        target='_blank'
+        onClick={() => handleClickSocialIcon('github')}
+      >
         <Image alt='Github' src={githubIcon} width={width} height={height} />
       </Link>
     </div>
