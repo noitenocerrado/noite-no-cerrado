d8517f71161f90c97b6809e4046048a5e7d508ac
Mateus Félix <mateus.cfoliveira@gmail.com>
2024-06-16 18:13:01 -0300

    feat: replace link component on QuickAccessCard

--- src/components/atoms/QuickAccessCard/QuickAccessCard.tsx
+++ src/components/atoms/QuickAccessCard/QuickAccessCard.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useTranslatedPath } from '@/hooks'
+import Link from 'next/link'
 
 export const QuickAccessCard = ({
   title,
@@ -11,13 +12,13 @@ export const QuickAccessCard = ({
   const fullPath = useTranslatedPath({ href })
 
   return (
-    <a
+    <Link
       href={fullPath}
       className='py-1 bg-zinc-900 rounded-lg border border-gray-400 shadow-md hover:bg-gray-700 transition flex justify-center'
     >
       <h2 className='text-lg font-bold tracking-tight text-gray-300'>
         {title}
       </h2>
-    </a>
+    </Link>
   )
 }
