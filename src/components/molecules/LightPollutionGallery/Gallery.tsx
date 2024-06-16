cc175a42f7092c568f3164004609788a64e9aa55
Mateus Félix <mateus.cfoliveira@gmail.com>
2024-06-16 14:51:14 -0300

    feat: implement mobile gallery and desktop version | add translated text

--- src/components/molecules/LightPollutionGallery/Gallery.tsx
+++ src/components/molecules/LightPollutionGallery/Gallery.tsx
@@ -1,56 +1,113 @@
 'use client'
-import { TranslationProps } from '@/types/translation'
 import Image from 'next/image'
+import { useState } from 'react'
+import { GalleryProps, MobileGallery } from './MobileGallery'
 
-interface GalleryProps extends TranslationProps {
-  images: {
+export const Gallery = ({ dictionary, images }: GalleryProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [selectedImage, setSelectedImage] = useState<{
     description: string
-    thumbnail: string
     original: string
-    blurDataURL: string
-  }[]
-}
+  }>({ description: '', original: '' })
 
-const MobileGallery = ({ dictionary, images }: GalleryProps) => (
-  <div className='flex overflow-x-scroll space-x-4 p-4 no-scrollbar md:hidden'>
-    {images.map(({ description, original, blurDataURL }) => (
-      <div key={description} className='min-w-[300px]'>
-        <div className='relative'>
-          <Image
-            alt={description}
-            width={1614}
-            height={1291}
-            src={original}
-            placeholder='blur'
-            blurDataURL={blurDataURL}
-            className='rounded-lg'
-          />
-        </div>
+  const openModal = (image: { description: string; original: string }) => {
+    setSelectedImage(image)
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setSelectedImage({ description: '', original: '' })
+  }
+
+  return (
+    <>
+      <p className='mb-5'>{dictionary.photosSection.pl_gallery.description}</p>
+
+      <p className='mb-5'>
+        {dictionary.photosSection.pl_gallery.exif.focalLength}24mm |{' '}
+        {dictionary.photosSection.pl_gallery.exif.aperture}f/4 |{' '}
+        {dictionary.photosSection.pl_gallery.exif.exposureTime}20s
+      </p>
+
+      <div className='md:hidden'>
+        <MobileGallery dictionary={dictionary} images={images} />
       </div>
-    ))}
-  </div>
-)
 
-export const Gallery = ({ dictionary, images }: GalleryProps) => (
-  <>
-    <MobileGallery dictionary={dictionary} images={images} />
+      <div className='hidden md:grid grid-cols-2 justify-items-center lg:grid-cols-3 gap-5 lg:gap-8'>
+        {images.map(({ description, original, blurDataURL }) => (
+          <div
+            key={description}
+            className='min-w-[200px] max-w-[300px] lg:max-w-[500px] relative group'
+            onClick={() => openModal({ description, original })}
+          >
+            <div className='relative cursor-pointer'>
+              <Image
+                alt={description}
+                width={1614}
+                height={1291}
+                src={original}
+                placeholder='blur'
+                blurDataURL={blurDataURL}
+                className='rounded-lg'
+              />
+              <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100'>
+                <div className='p-4 text-center'>
+                  <p className='text-sm lg:text-xs xl:text-sm'>
+                    {
+                      // @ts-ignore
+                      dictionary.photosSection.pl_gallery[description]
+                        .descriptionDesktop
+                    }
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
 
-    <div className='hidden md:grid grid-cols-2 justify-items-center gap-5 lg:grid-cols-4 lg:gap-8'>
-      {images.map(({ description, original, blurDataURL }) => (
-        <div key={description} className='min-w-[200px] max-w-[300px]'>
-          <div className='relative'>
-            <Image
-              alt={description}
-              width={1614}
-              height={1291}
-              src={original}
-              placeholder='blur'
-              blurDataURL={blurDataURL}
-              className='rounded-lg'
-            />
+      {isModalOpen && Boolean(selectedImage.original) && (
+        <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75'>
+          <div className='relative bg-zinc-900 rounded-lg shadow-lg max-w-[800px] mx-auto p-8'>
+            <button
+              onClick={closeModal}
+              className='absolute top-2 right-2 text-stone-300 hover:text-gray-500 transition-colors duration-300'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M6 18L18 6M6 6l12 12'
+                />
+              </svg>
+            </button>
+            <figure>
+              <Image
+                alt={selectedImage.description}
+                width={1614}
+                height={1291}
+                src={selectedImage.original}
+                className='rounded-lg'
+              />
+              <figcaption className='mt-4 text-center text-lg text-stone-300'>
+                {
+                  // @ts-ignore
+                  dictionary.photosSection.pl_gallery[selectedImage.description]
+                    .descriptionDesktop
+                }
+              </figcaption>
+            </figure>
           </div>
         </div>
-      ))}
-    </div>
-  </>
-)
+      )}
+    </>
+  )
+}
--- src/components/molecules/LightPollutionGallery/LightPollutionGallery.tsx
+++ src/components/molecules/LightPollutionGallery/LightPollutionGallery.tsx
@@ -1,34 +1,34 @@
-'use client'
-
-import ImageGallery from 'react-image-gallery'
-import 'react-image-gallery/styles/css/image-gallery.css'
-import { TranslationProps } from '../../../types/translation'
+'use server'
+import { TranslationProps } from '@/types/translation'
+import { getBase64Image } from '@/utils'
 import { images } from './images'
-import Image from 'next/image'
+import { Gallery } from './Gallery'
 
-export const LightPollutionGallery = ({ dictionary }: TranslationProps) => (
-  <div className='w-full flex justify-center items-center flex-col'>
-    <p className='mb-5'>{dictionary.photosSection.pl_gallery.description}</p>
-    <p className='mb-2 text-xl'>EXIF</p>
-    <ul className='mb-5'>
-      <li>{dictionary.photosSection.pl_gallery.exif.focalLength}24mm</li>
-      <li>ISO 4000</li>
-      <li>{dictionary.photosSection.pl_gallery.exif.aperture}f/4</li>
-      <li>{dictionary.photosSection.pl_gallery.exif.exposureTime}20s</li>
-    </ul>
-    <div className='xs:w-full sm:w-1/2'>
-      <ImageGallery
-        items={images}
-        renderItem={({ original, description }) => (
-          <div className='w-full pb-5 flex flex-col items-center justify-center'>
-            <div className='w-full flex justify-center relative'>
-              <Image alt='' width={800} height={100} src={original} />
-            </div>
-            {/* @ts-ignore */}
-            <p className='mt-4'>{`${dictionary.photosSection.pl_gallery[description].description}`}</p>
-          </div>
-        )}
-      />
+interface Images {
+  description: string
+  thumbnail: string
+  original: string
+  blurDataURL: string
+}
+
+const getImages = async (): Promise<Images[]> => {
+  const imagesWithBlurData = await Promise.all(
+    images.map(async (image) => {
+      const blurDataURL = await getBase64Image(image.thumbnail)
+      return { ...image, blurDataURL }
+    })
+  )
+  return imagesWithBlurData
+}
+
+export const LightPollutionGallery = async ({
+  dictionary,
+}: TranslationProps) => {
+  const images = await getImages()
+
+  return (
+    <div className='gallery'>
+      <Gallery images={images} dictionary={dictionary} />
     </div>
-  </div>
-)
+  )
+}
